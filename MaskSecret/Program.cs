8a135412655b5fa4892fd1a3766c0daca93944d5
John Thielman <john.thielman@gmail.com>
2020-03-02 16:27:40 -0600

    Add project files.

---
+++ MaskSecret/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MaskSecret
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your secret?");
+            var secret = Console.ReadLine();
+            var result = secret.Substring(secret.Length - 4).PadLeft(secret.Length, '*');
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
